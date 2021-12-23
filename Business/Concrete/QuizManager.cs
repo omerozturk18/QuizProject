f53d78111ae5592847665e274710bf99d74b254b
Ömer Öztürk <46124907+omerozturk18@users.noreply.github.com>
2021-12-23 11:46:13 +0300

    Update QuizManager.cs
    GetQuizDetail, ActivatedQuiz, StartedQuiz, CompletedQuiz Functions Added

--- Business/Concrete/QuizManager.cs
+++ Business/Concrete/QuizManager.cs
@@ -20,21 +20,31 @@ namespace Business.Concrete
 
         public IResult Add(Quiz entity)
         {
+            entity.QuizNumber =new Random(Guid.newGuid().GetHashCode()).Next(0, 10);
+            entity.OperationDate =Date.Now();
+            entity.Status=Status.PASSIVE;
             _quizDal.Add(entity);
             return new SuccessResult(Messages.Added);
         }
 
+        [SecuredOperation("admin")]
         public IResult Delete(Quiz entity)
         {
-            _quizDal.Delete(entity);
-            return new SuccessResult(Messages.Added);
+            entity.OperationDate =Date.Now();
+            entity.Status=Status.DELETED;
+            _quizDal.Update(entity);
+            return new SuccessResult(Messages.Deleted);
+           /* _quizDal.Delete(entity);
+            return new SuccessResult(Messages.Deleted);*/
         }
 
+        [SecuredOperation("admin")]
         public IDataResult<List<Quiz>> GetAll()
         {
             return new SuccessDataResult<List<Quiz>>(_quizDal.GetAll(), Messages.Listed);
         }
-
+        
+        [SecuredOperation("admin")]
         public IDataResult<Quiz> GetById(int id)
         {
             return new SuccessDataResult<Quiz>(_quizDal.Get(c => c.Id == id), Messages.Listed);
@@ -43,7 +53,44 @@ namespace Business.Concrete
         public IResult Update(Quiz entity)
         {
             _quizDal.Update(entity);
-            return new SuccessResult(Messages.Added);
+            return new SuccessResult(Messages.Updated);
+        }
+
+        public IDataResult<Quiz> IsThereQuiz(string quizNumber)
+        {
+            return new SuccessDataResult<Quiz>(_quizDal.Get(c => c.QuizNumber == quizNumber && c.Status == Status.ACTIVE), Messages.Listed);
+        }
+
+        public IDataResult<QuizDto> GetQuizDetail(int id)
+        {
+            return new SuccessDataResult<QuizDto>(_quizDal.GetQuizDetail(c => c.Id == id), Messages.Listed);
+        }
+        
+        public IResult ActivatedQuiz(int id)
+        {
+            var quiz = _quizDal.Get(c => c.Id == id && c.Status == Status.PASSIVE);
+            if(quiz == null) return new ErrorResult(Messages.NotQuiz);
+            quiz.Status=Status.ACTIVE;
+            _quizDal.Update(quiz);
+            return new SuccessResult(Messages.Started);
+        }
+
+         public IResult StartedQuiz(int id)
+        {
+            var quiz = _quizDal.Get(c => c.Id == id && c.Status == Status.ACTIVE);
+            if(quiz == null) return new ErrorResult(Messages.NotQuiz);
+            quiz.Status=Status.STARTED;
+            _quizDal.Update(quiz);
+            return new SuccessResult(Messages.Started);
+        }
+
+         public IResult CompletedQuiz(int id)
+        {
+            var quiz = _quizDal.Get(c => c.Id == id && c.Status == Status.STARTED);
+            if(quiz == null) return new ErrorResult(Messages.NotQuiz);
+            quiz.Status=Status.COMPLETED;
+            _quizDal.Update(quiz);
+            return new SuccessResult(Messages.Started);
         }
     }
 }
