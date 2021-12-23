3b1149884824a2e9a0343921b9a096e48c5860c5
Ömer Öztürk <46124907+omerozturk18@users.noreply.github.com>
2021-12-23 14:33:02 +0300

    Update EfQuizDal.cs

--- DataAccess/Concrete/EntityFramework/EfQuizDal.cs
+++ DataAccess/Concrete/EntityFramework/EfQuizDal.cs
@@ -10,10 +10,10 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfQuizDal : EfEntityRepositoryBase<Quiz, QuizContext>, IQuizDal
     {
-      public QuizDto GetQuizDetail(Expression<Func<QuizDto, bool>> filter = null){
+       public QuizDto GetQuizDetail(int id){
           using (QuizContext context= new QuizContext())
             {
-                var result = from car in context.Quizes
+                var result = from car in context.Quizzes
                             /* join b in context.Brands on car.BrandId equals b.BrandId
                              join c in context.Colors on car.ColorId equals c.ColorId
                              join cI in context.CarImages on car.CarId equals cI.CarId into gr
@@ -21,10 +21,28 @@ namespace DataAccess.Concrete.EntityFramework
                              select new QuizDto
                              {
                              };
+
+
+                return result;
+            }
+       }
+       public List<QuizDto> GetAllQuizzesDetail(Expression<Func<QuizDto, bool>> filter = null){
+          using (QuizContext context= new QuizContext())
+            {
+                var result = from car in context.Quizzes
+                            /* join b in context.Brands on car.BrandId equals b.BrandId
+                             join c in context.Colors on car.ColorId equals c.ColorId
+                             join cI in context.CarImages on car.CarId equals cI.CarId into gr
+                             from cI in gr.DefaultIfEmpty()*/
+                             select new QuizDto
+                             {
+                             };
+
+
                 return filter==null
                 ?result.ToList()
                 :result.Where(filter).ToList();
-              }
-      }
+            }
+       }
     }
 }
