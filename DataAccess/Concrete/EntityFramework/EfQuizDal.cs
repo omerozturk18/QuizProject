5ef5ae722f81777c7fef2c4ffdf5ad36a18b90e3
Ömer Öztürk <46124907+omerozturk18@users.noreply.github.com>
2021-12-23 11:53:57 +0300

    Update EfQuizDal.cs

--- DataAccess/Concrete/EntityFramework/EfQuizDal.cs
+++ DataAccess/Concrete/EntityFramework/EfQuizDal.cs
@@ -10,5 +10,18 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfQuizDal : EfEntityRepositoryBase<Quiz, QuizContext>, IQuizDal
     {
+      public QuizDto GetQuizDetail(int id){
+          using (QuizContext context= new QuizContext())
+            {
+                var result = from car in context.Quizes
+                            /* join b in context.Brands on car.BrandId equals b.BrandId
+                             join c in context.Colors on car.ColorId equals c.ColorId
+                             join cI in context.CarImages on car.CarId equals cI.CarId into gr
+                             from cI in gr.DefaultIfEmpty()*/
+                             select new QuizDto
+                             {
+                             };
+                return result;
+      }
     }
 }
