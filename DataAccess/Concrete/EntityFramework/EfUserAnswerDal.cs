53b8ec858b5207662f61beb3bf2025485d6510d7
Ömer Öztürk <46124907+omerozturk18@users.noreply.github.com>
2021-12-23 14:25:09 +0300

    Update EfUserAnswerDal.cs

--- DataAccess/Concrete/EntityFramework/EfUserAnswerDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUserAnswerDal.cs
@@ -10,7 +10,23 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfUserAnswerDal : EfEntityRepositoryBase<UserAnswer, QuizContext>, IUserAnswerDal
     {
-       public UserAnswerDto GetAnswerDetail(Expression<Func<UserAnswerDto, bool>> filter = null){
+       public UserAnswerDto GetAnswerDetail(int id){
+          using (QuizContext context= new QuizContext())
+            {
+                var result = from car in context.UserAnswers
+                            /* join b in context.Brands on car.BrandId equals b.BrandId
+                             join c in context.Colors on car.ColorId equals c.ColorId
+                             join cI in context.CarImages on car.CarId equals cI.CarId into gr
+                             from cI in gr.DefaultIfEmpty()*/
+                             select new UserAnswerDto
+                             {
+                             };
+
+
+                return result;
+            }
+       }
+       public List<UserAnswerDto> GetAnswersDetail(Expression<Func<UserAnswerDto, bool>> filter = null){
           using (QuizContext context= new QuizContext())
             {
                 var result = from car in context.UserAnswers
