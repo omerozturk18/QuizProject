223ba5416d2dd09a865c21b9d04c592038a9d581
Ömer Öztürk <46124907+omerozturk18@users.noreply.github.com>
2021-12-23 14:33:18 +0300

    Update EfQuestionDal.cs

--- DataAccess/Concrete/EntityFramework/EfQuestionDal.cs
+++ DataAccess/Concrete/EntityFramework/EfQuestionDal.cs
@@ -10,8 +10,8 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfQuestionDal : EfEntityRepositoryBase<Question, QuizContext>, IQuestionDal
     {
-       public QuestionDto GetQuestionDetail(Expression<Func<QuestionDto, bool>> filter = null){
-           using (QuizContext context= new QuizContext())
+       public QuestionDto GetQuestionDetail(int id){
+          using (QuizContext context= new QuizContext())
             {
                 var result = from car in context.Questions
                             /* join b in context.Brands on car.BrandId equals b.BrandId
@@ -21,8 +21,28 @@ namespace DataAccess.Concrete.EntityFramework
                              select new QuestionDto
                              {
                              };
+
+
                 return result;
-              }
+            }
+       }
+       public List<QuestionDto> GetQuestionsDetail(Expression<Func<QuestionDto, bool>> filter = null){
+          using (QuizContext context= new QuizContext())
+            {
+                var result = from car in context.Questions
+                            /* join b in context.Brands on car.BrandId equals b.BrandId
+                             join c in context.Colors on car.ColorId equals c.ColorId
+                             join cI in context.CarImages on car.CarId equals cI.CarId into gr
+                             from cI in gr.DefaultIfEmpty()*/
+                             select new QuestionDto
+                             {
+                             };
+
+
+                return filter==null
+                ?result.ToList()
+                :result.Where(filter).ToList();
+            }
        }
     }
 }
