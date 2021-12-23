eb3ff0cbef0eed71e103e5af98495db06ab5db7c
Ömer Öztürk <46124907+omerozturk18@users.noreply.github.com>
2021-12-23 14:07:24 +0300

    Update EfUserAnswerDal.cs

--- DataAccess/Concrete/EntityFramework/EfUserAnswerDal.cs
+++ DataAccess/Concrete/EntityFramework/EfUserAnswerDal.cs
@@ -10,5 +10,80 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfUserAnswerDal : EfEntityRepositoryBase<UserAnswer, QuizContext>, IUserAnswerDal
     {
+       public UserAnswerDto GetAnswerDetail(Expression<Func<UserAnswerDto, bool>> filter = null){
+          using (QuizContext context= new QuizContext())
+            {
+                var result = from car in context.UserAnswers
+                            /* join b in context.Brands on car.BrandId equals b.BrandId
+                             join c in context.Colors on car.ColorId equals c.ColorId
+                             join cI in context.CarImages on car.CarId equals cI.CarId into gr
+                             from cI in gr.DefaultIfEmpty()*/
+                             select new UserAnswerDto
+                             {
+                             };
+
+
+                return filter==null
+                ?result.ToList()
+                :result.Where(filter).ToList();
+            }
+       }
+       
+       public List<UserAnswerDto> GetUserAnswersDetail(Expression<Func<UserAnswerDto, bool>> filter = null){
+          using (QuizContext context= new QuizContext())
+            {
+                var result = from car in context.UserAnswer
+                            /* join b in context.Brands on car.BrandId equals b.BrandId
+                             join c in context.Colors on car.ColorId equals c.ColorId
+                             join cI in context.CarImages on car.CarId equals cI.CarId into gr
+                             from cI in gr.DefaultIfEmpty()*/
+                             select new UserAnswerDto
+                             {
+                             };
+
+
+                return filter==null
+                ?result.ToList()
+                :result.Where(filter).ToList();
+            }
+       }
+       
+       public List<UserAnswerDto> GetQuestionAnswersDetail(Expression<Func<UserAnswerDto, bool>> filter = null)){
+          using (QuizContext context= new QuizContext())
+            {
+                var result = from car in context.UserAnswer
+                            /* join b in context.Brands on car.BrandId equals b.BrandId
+                             join c in context.Colors on car.ColorId equals c.ColorId
+                             join cI in context.CarImages on car.CarId equals cI.CarId into gr
+                             from cI in gr.DefaultIfEmpty()*/
+                             select new UserAnswerDto
+                             {
+                             };
+
+
+                return filter==null
+                ?result.ToList()
+                :result.Where(filter).ToList();
+            }
+       }
+       
+       public List<UserAnswerDto> GetQuizAnswersDetail(Expression<Func<UserAnswerDto, bool>> filter = null){
+          using (QuizContext context= new QuizContext())
+            {
+                var result = from car in context.UserAnswer
+                            /* join b in context.Brands on car.BrandId equals b.BrandId
+                             join c in context.Colors on car.ColorId equals c.ColorId
+                             join cI in context.CarImages on car.CarId equals cI.CarId into gr
+                             from cI in gr.DefaultIfEmpty()*/
+                             select new UserAnswerDto
+                             {
+                             };
+
+
+                return filter==null
+                ?result.ToList()
+                :result.Where(filter).ToList();
+            }
+       }
     }
 }
